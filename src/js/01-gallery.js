da9948f9ee10422ae1953949cc8bbe579ad36e99
Agnieszka Lewandowska Cieślak <agnieszka.lewandowska.cieslak@gmail.com>
2023-03-18 13:22:43 +0100

    again

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,23 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryMarker = document.querySelector('ul.gallery');
+
+galleryItems.forEach(item => {
+  const galleryItem = `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
+</a>`;
+  galleryMarker.insertAdjacentHTML('beforeend', galleryItem);
+});
+
+const handleClick = e => {
+  e.preventDefault();
+  e.stopPropagation();
+  new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+  });
+};
+
+galleryMarker.addEventListener('click', handleClick);
